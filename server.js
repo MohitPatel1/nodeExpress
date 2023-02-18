998fd26672cca1974169c26fbbba680d0628c024
Mohit <mohit.patel1966@gmail.com>
2023-02-18 08:58:16 +0530

    19.1

--- server.js
+++ server.js
@@ -16,12 +16,28 @@ let routes = require('./routes')
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
 
 app.set('view engine','pug')
 app.set('views','./views/pug')
@@ -41,7 +57,7 @@ mongo.connect(uri, { useUnifiedTopology: true },(err, client) => {
     io.on('connection', socket => {
       ++currentUsers;
       io.emit('user count', currentUsers);
-      console.log('A user has connected');
+      console.log('user ' + socket.request.user.username + ' connected');
       socket.on('disconnect', () => {
         console.log("A user has discommected")
         --currentUsers;
@@ -53,5 +69,16 @@ mongo.connect(uri, { useUnifiedTopology: true },(err, client) => {
     auth(app, db, session, passport, ObjectId, LocalStrategy, bcrypt)
 
     routes(app, db, passport, bodyParser, bcrypt, http)
+  }
+  function onAuthorizeSuccess(data, accept) {
+    console.log('successful connection to socket.io');
+  
+    accept(null, true);
+  }
+  
+  function onAuthorizeFail(data, message, error, accept) {
+    if (error) throw new Error(message);
+    console.log('failed connection to socket.io:', message);
+    accept(null, false);
   }
 })
