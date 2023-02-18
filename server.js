393e86e1de0a3a7f4dec9896aed2c45beaa5765e
Mohit <mohit.patel1966@gmail.com>
2023-02-18 09:16:46 +0530

    19.4

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ let routes = require('./routes')
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const passportSocketIo = require('passport.socketio')
-const cookieParser = require('cookie-parser')
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
 const MongoStore = require('connect-mongo')(session);
 const URI = process.env.MONGO_URI;
 const store = new MongoStore({ url: URI });
@@ -28,17 +28,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-function onAuthorizeSuccess(data, accept) {
-  console.log('successful connection to socket.io');
-
-  accept(null, true);
-}
-
-function onAuthorizeFail(data, message, error, accept) {
-  if (error) throw new Error(message);
-  console.log('failed connection to socket.io:', message);
-  accept(null, false);
-}
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
+}));
 
 io.use(
   passportSocketIo.authorize({
@@ -82,6 +79,18 @@ mongo.connect(uri, { useUnifiedTopology: true },(err, client) => {
     auth(app, db, session, passport, ObjectId, LocalStrategy, bcrypt)
 
     routes(app, db, passport, bodyParser, bcrypt, http)
+  }
+
+  function onAuthorizeSuccess(data, accept) {
+    console.log('successful connection to socket.io');
+  
+    accept(null, true);
+  }
+  
+  function onAuthorizeFail(data, message, error, accept) {
+    if (error) throw new Error(message);
+    console.log('failed connection to socket.io:', message);
+    accept(null, false);
   }
 
   const PORT = process.env.PORT || 3000;
